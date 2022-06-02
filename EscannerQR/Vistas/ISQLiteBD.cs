439842ede265891b83a3b141a40107619f60e45b
Dio1295 <daviddionicioc4@gmail.com>
2022-06-02 10:25:07 -0500

    Add files via upload

---
+++ EscannerQR/Vistas/ISQLiteBD.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EscannerQR.Vistas
+{
+    public interface ISQLiteBD
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
