42895c4e212b3dd0a92fdcc1b44de271d6c13b34
Dio1295 <daviddionicioc4@gmail.com>
2022-06-02 10:38:08 -0500

    Add files via upload

---
+++ EscannerQR/EscannerQR.Android/SQLiteBD.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EscannerQR.Droid;
+using Xamarin.Forms;
+using SQLite;
+using System.IO;
+using EscannerQR.Vistas;
+[assembly: Dependency(typeof(SQLiteBD))]
+
+namespace EscannerQR.Droid
+{
+    public class SQLiteBD : ISQLiteBD
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+            var db = new SQLiteAsyncConnection(path);
+            
+            return new SQLiteAsyncConnection(path);
+           
+        }
+    }
+}
